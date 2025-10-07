2fd0a7dc71538601a277a93ceb553f6021a020e5
Yumin Chu <yuminc03@gmail.com>
2025-10-08 00:22:38 +0900

    Feat: 6주차 - 모듈을 불러오는 문법

---
+++ 모듈불러오기.py
@@ -0,0 +1,14 @@
+# 모듈 불러오는 방법
+# 1. import 모듈명
+# import turtle
+# turtle.forward(100) # turtle.fd(100)
+
+# 2. import 모듈명 as 별칭
+# import turtle as t
+# t.forward(100)
+
+# 3. from 모듈명 import *
+from turtle import *
+shape('turtle')
+color('red')
+forward(100)
