d8348732e03714ea51b4770a2a3fdbbaa8f845cf
Yumin Chu <yuminc03@gmail.com>
2025-10-08 00:25:04 +0900

    Feat: 6주차 - 순차형 터틀 동작 (선 3개를 각각 다른 색으로 그리기)

---
+++ 6-5.py
@@ -0,0 +1,18 @@
+# 6-5: 순차형 터틀 동작
+from turtle import *
+shape('turtle')
+bgcolor('black')
+
+color('green')
+pensize(5)
+fd(100)
+right(60)
+
+color('yellow')
+pensize(1)
+fd(50)
+left(60)
+
+color('purple')
+pensize(5)
+fd(100)
