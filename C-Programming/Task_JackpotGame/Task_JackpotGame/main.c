710b4e76c9c511105f0ffd62281eaab967d51fae
Yumin Chu <yuminc03@gmail.com>
2025-11-09 23:35:12 +0900

    feat: 잭팟 게임 기본 기능 구현

--- C-Programming/Task_JackpotGame/Task_JackpotGame/main.c
+++ C-Programming/Task_JackpotGame/Task_JackpotGame/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
+	srand(time(NULL));
+	int count = 0;
 
+	while (1)
+	{
+		count += 1;
+		printf("*%d회 게임*\n", count);
+		// rand() % 개수
+		int a = (rand() % 7) + 1;
+		int b = (rand() % 7) + 1;
+		int c = (rand() % 7) + 1;
+		printf("a: %d\n", a);
+		printf("b: %d\n", b);
+		printf("c: %d\n", c);
+
+		if (a == 7 && b == 7 && c == 7)
+		{
+			printf("잭팟 성공!\n");
+			printf("시도 횟수: %d\n", count);
+			break;
+		}
+		else {
+			printf("다음 기회에..\n");
+		} // end if
+	}
+	
 	return 0;
 }
