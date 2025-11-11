75974c7381ad951da0a1c18ac562a5beba083579
Yumin Chu <yuminc03@gmail.com>
2025-11-11 22:12:21 +0900

    feat: 7주차 - 증감 연산자, 삼항 연산자

---
+++ C-Programming/Project_1018/Project_1018/incrementDecreementOperation.c
@@ -0,0 +1,17 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int a = 10;
+//	int x;
+//	x = a++;
+//
+//	int b = 10;
+//	int y;
+//	y = ++b;
+//
+//	printf("%d\n", x); // 10
+//	printf("%d\n", y); // 11
+//
+//	return 0;
+//}
